196ef0809d2ff622cc6f5047fc4f70dc02995694
Sultan Alahmadi <su.ahmadi@outlook.com>
2022-04-29 03:51:22 -0400

    uploaded new database with more data

---
+++ database/database_dump_april.sql
@@ -0,0 +1,137 @@
+CREATE DATABASE  IF NOT EXISTS `covidtravel_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `covidtravel_db`;
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: covidtravel_db
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `airlines`
+--
+
+DROP TABLE IF EXISTS `airlines`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `airlines` (
+  `airline_id` int NOT NULL,
+  `airline_name` varchar(45) DEFAULT NULL,
+  `mask_required` tinyint DEFAULT NULL,
+  `vaccine_required` tinyint DEFAULT NULL,
+  PRIMARY KEY (`airline_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `airlines`
+--
+
+LOCK TABLES `airlines` WRITE;
+/*!40000 ALTER TABLE `airlines` DISABLE KEYS */;
+INSERT INTO `airlines` VALUES (1,'Virgin',1,1),(2,'Jet Blue',1,1),(3,'Etihad',1,1),(4,'Southwest',1,1),(5,'Atlantic',1,1),(6,'American Airlines',1,1),(7,'Delta',1,1),(8,'United Airlines',1,1),(9,'Alaska Airlines',0,0),(10,'Spirit',0,0),(11,'Envoy Air',1,0),(12,'Silver Airways',0,1),(13,'Cape Air',0,0),(14,'Elite Airways',0,1),(15,'Endeavor Air',1,0),(16,'GoJet Airlines',0,1),(17,'Horizon Air',0,0),(18,'Mesa Airlines',1,1),(19,'Piedmont Airlines',0,1),(20,'PSA Airlines',1,0),(21,'Republic Airways',1,1),(22,'Silver Airways',0,1),(23,'SkyWest Airlines',1,0),(24,'Air Choice One',1,0),(25,'Air Sunshine',1,1),(26,'Bering Air',0,0),(27,'Boutique Air',0,1),(28,'Everts Air',0,1),(29,'Frontier Flying Service',0,1),(30,'Gem Air',0,0),(31,'Grand Canyon Airlines',0,0),(32,'Grand Canyon Scenic Airlines',0,0),(33,'Grant Aviation',0,0),(34,'Griffing Flying Service',0,0),(35,'Hageland Aviation Services',1,1),(36,'Island Airways',1,0),(37,'JSX',1,1);
+/*!40000 ALTER TABLE `airlines` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `countries`
+--
+
+DROP TABLE IF EXISTS `countries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `countries` (
+  `country_name` varchar(100) NOT NULL,
+  `vaccine_required` tinyint DEFAULT NULL,
+  `testing_required` tinyint DEFAULT NULL,
+  `risk_level` int DEFAULT NULL,
+  `quarantine_required` tinyint DEFAULT NULL,
+  PRIMARY KEY (`country_name`),
+  KEY `index_country` (`country_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `countries`
+--
+
+LOCK TABLES `countries` WRITE;
+/*!40000 ALTER TABLE `countries` DISABLE KEYS */;
+INSERT INTO `countries` VALUES ('Afghanistan',1,1,1,1),('Bahamas',1,0,0,0),('Belize',0,1,0,0),('Canada',1,0,1,0),('Costa Rica',0,0,0,0),('Croatia',0,0,0,0),('Cuba',0,0,0,0),('Cyprus',0,0,0,0),('Denmark',0,1,0,0),('Egypt',1,1,0,1),('France',0,1,0,0),('Greece',0,0,0,0),('Iceland',0,0,0,0),('Iran',1,1,1,1),('Iraq',1,1,0,1),('Ireland',0,1,0,0),('Italy',0,1,0,0),('Luxembourg',0,1,0,0),('Maldives',0,1,0,0),('Mexico',1,0,1,0),('Monaco',0,1,0,0),('Norway',0,0,0,0),('Oman',1,1,1,1),('Portugal',0,0,0,0),('Qatar',1,1,0,0),('Saudi Arabia',0,0,0,0),('Seychelles',0,1,0,0),('Slovenia',0,0,0,0),('South Afriaca',0,1,0,0),('Spain',0,0,0,0),('Sri Lanka',0,1,0,0),('Sweden',0,0,0,0),('Turkey',0,1,0,0),('UAE',1,1,1,1),('United Kingdom',1,0,0,0),('Yemen',1,0,1,0);
+/*!40000 ALTER TABLE `countries` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `lastName` text NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `passport` text NOT NULL,
+  `vaccine_status` text NOT NULL,
+  `vaccine_type` text NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Sultan','A','su.ahmadi@outlook.com','testPassword','Saudi Arabia','Vaccinated','Pfizer');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vaccines`
+--
+
+DROP TABLE IF EXISTS `vaccines`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vaccines` (
+  `vaccine_id` int NOT NULL,
+  `vaccine_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`vaccine_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vaccines`
+--
+
+LOCK TABLES `vaccines` WRITE;
+/*!40000 ALTER TABLE `vaccines` DISABLE KEYS */;
+INSERT INTO `vaccines` VALUES (1,'Pfizer'),(2,'Moderna'),(3,'J&J'),(4,'Other');
+/*!40000 ALTER TABLE `vaccines` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-29  3:48:41
